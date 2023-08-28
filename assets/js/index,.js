8de5f034d09fa88e8ebdfb0bbc6d3656fa0aa2d3
Rushikesh Shelar <112684561+RushikeshShelar@users.noreply.github.com>
2023-08-28 22:00:57 +0530

    Removed Junk File

--- assets/js/index,.js
+++
@@ -1,9 +0,0 @@
-//Toggle Navbar Icon
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-};
-
